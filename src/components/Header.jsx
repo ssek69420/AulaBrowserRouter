90f1a17cee946697b6b2b24f5619e278fbb724ba
ssek69420 <kaikevix2233@gmail.com>
2024-08-10 17:14:09 -0400

    added carousel
    not working!! fix pls

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom";
+import { Carousel } from "react-responsive-carousel"
+
 
 export default function Header(){
     return(
+<>
+    <Carousel/>
         <header>
             <Link to='/home'>
             <p>Home</p>
@@ -13,5 +17,6 @@ export default function Header(){
                 <p>Offers</p>
             </Link>
         </header>
+</>
     )
 }
