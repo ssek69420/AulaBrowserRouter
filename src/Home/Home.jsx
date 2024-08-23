db49dcf0d79a57df2cbd64ed3b97bc0413e28ff0
ssek69420 <kaikevix2233@gmail.com>
2024-08-23 01:44:18 -0300

    fix home.jsx

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -6,11 +6,11 @@ import ListProducts from "../components/ListProducts";
 
 export default function Home(){
     const [productsList] = useState([
-        { id: 1, title: "AZ Series", sign: "Marcelino's choice",  price: '$6000.00', img: 'https://www.ibanez.com/common/product_artist_file/file/pm_thum_eg_az_en.png', discount: true },
-        { id: 2, title: 'AZES Series', sign: "Azali's choice",  price: '$2300.23', img: 'https://www.ibanez.com/common/product_artist_file/file/pm_thum_eg_azes_en.jpg', discount: true },
-        { id: 3, title: 'AZS Series', sign: "Van Halen's choice",  price: '$3000.00', img: 'https://www.ibanez.com/common/product_artist_file/file/pm_thum_eg_azs_en.jpg', discount: true},
-        { id: 4, title: 'RG Series', sign: "Scott Lepage's choice", price: '$8999.00', img: 'https://www.ibanez.com/common/product_artist_file/file/pm_thum_eg_rg_en.jpg', discount: true},
-        { id: 5, title: 'Q Series', sign: "Ichika Nito's choice", price: '$2930.00', img: 'https://www.ibanez.com/common/product_artist_file/file/pm_thum_eg_q_eu.jpg', discount: true}
+        { id: 1, title: "AZ Series", sign: "Marcelino's choice",  realprice: '$6000.00', img: 'https://www.ibanez.com/common/product_artist_file/file/pm_thum_eg_az_en.png', discount: true },
+        { id: 2, title: 'AZES Series', sign: "Azali's choice",  realprice: '$2300.23', img: 'https://www.ibanez.com/common/product_artist_file/file/pm_thum_eg_azes_en.jpg', discount: true },
+        { id: 3, title: 'AZS Series', sign: "Van Halen's choice",  realprice: '$3000.00', img: 'https://www.ibanez.com/common/product_artist_file/file/pm_thum_eg_azs_en.jpg', discount: true},
+        { id: 4, title: 'RG Series', sign: "Scott Lepage's choice", realprice: '$8999.00', img: 'https://www.ibanez.com/common/product_artist_file/file/pm_thum_eg_rg_en.jpg', discount: true},
+        { id: 5, title: 'Q Series', sign: "Ichika Nito's choice", realprice: '$2930.00', img: 'https://www.ibanez.com/common/product_artist_file/file/pm_thum_eg_q_eu.jpg', discount: true}
     ]);
 
     const [partnersList] = useState([
